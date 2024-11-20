655e33bd886b9311a764676ea6df63f93b689b02
daniele.gubbiotti <gubbiottidaniele@gmail.com>
2024-11-20 12:13:25 +0100

    Initial Hub Core

---
+++ hub.core/hub.core/src/main/java/hub/learning/spring/hub/core/HubCoreApplication.java
@@ -0,0 +1,13 @@
+package hub.learning.spring.hub.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubCoreApplication.class, args);
+	}
+
+}
---
+++ hub.core/hub.core/src/test/java/hub/learning/spring/hub/core/HubCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.learning.spring.hub.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
